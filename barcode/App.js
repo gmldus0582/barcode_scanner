316ee89e3c666245bf57ebf63cf41bb4c07690c7
gmldus0582 <gmldus0582@gmail.com>
2021-02-04 13:53:50 +0900

    navigate 성공

--- barcode/App.js
+++ barcode/App.js
@@ -1,61 +1,60 @@
 import React, { Component } from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import {
   AppRegistry,
   StyleSheet,
-  Text,
   TouchableOpacity,
   Linking
 } from 'react-native';
-
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import { RNCamera } from 'react-native-camera';
+import { useNavigation } from '@react-navigation/native';
 
-const ScanScreen = () => {
-  onSuccess = e => {
-    Linking.openURL(e.data).catch(err =>
-      console.error('An error occured', err)
+
+onSuccess = e => {
+  Linking.openURL(e.data).catch(err =>
+    console.error('An error occured', err)
+  );
+  }
+  const HomeScreen = () => {
+    const navigation = useNavigation()
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Seonoh Home Screen</Text>
+        <Button
+          title='Go detail screen'
+          onPress={() => navigation.navigate('Details')} />
+      </View>
     );
-  };
+  }
+
+  const DetailsScreen = () => {
     return (
       <QRCodeScanner
         onRead={this.onSuccess}
         flashMode={RNCamera.Constants.FlashMode.torch}
-        topContent={
-          <Text style={styles.centerText}>
-            Go to{' '}
-            <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
-            your computer and scan the QR code.
-          </Text>
-        }
-        bottomContent={
-          <TouchableOpacity style={styles.buttonTouchable}>
-            <Text style={styles.buttonText}>OK. Got it!</Text>
-          </TouchableOpacity>
-        }
       />
     );
-  
-}
-
-const styles = StyleSheet.create({
-  centerText: {
-    flex: 1,
-    fontSize: 18,
-    padding: 32,
-    color: '#777'
-  },
-  textBold: {
-    fontWeight: '500',
-    color: '#000'
-  },
-  buttonText: {
-    fontSize: 21,
-    color: 'rgb(0,122,255)'
-  },
-  buttonTouchable: {
-    padding: 16
   }
-});
 
-export default ScanScreen;
+  const Stack = createStackNavigator();
+
+  const App = () => {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
+
+
+  export default App;
+
+   
