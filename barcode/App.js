693ac71e5a50607702aafdadb5f46d8125488c7b
gmldus0582 <gmldus0582@gmail.com>
2021-02-04 16:41:26 +0900

    url에서 바코드 연결

--- barcode/App.js
+++ barcode/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button,Dimensions, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { WebView } from 'react-native-webview';
 
 import {
   AppRegistry,
@@ -14,47 +15,51 @@ import { RNCamera } from 'react-native-camera';
 import { useNavigation } from '@react-navigation/native';
 
 
-onSuccess = e => {
-  Linking.openURL(e.data).catch(err =>
-    console.error('An error occured', err)
-  );
-  }
-  const HomeScreen = () => {
-    const navigation = useNavigation()
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Seonoh Home Screen</Text>
-        <Button
-          title='Go detail screen'
-          onPress={() => navigation.navigate('Details')} />
-      </View>
-    );
-  }
 
-  const DetailsScreen = () => {
-    return (
-      <QRCodeScanner
-        onRead={this.onSuccess}
-        flashMode={RNCamera.Constants.FlashMode.torch}
+const charwidth = Dimensions.get('window').width;
+const charheight= Dimensions.get('window').height;
+
+const HomeScreen = () => {
+  const navigation = useNavigation()
+  return (
+        <WebView
+        
+        onMessage={(event)=>navigation.navigate('Details')}
+        source={{uri: 'http://ip0139.cafe24.com/'}}
+        style={{marginTop: 20}}
+         
       />
-    );
+  );
+}
+
+function DetailsScreen() {
+  onSuccess = e => {
+    console.log(e)
   }
+  return (
+    
+    <QRCodeScanner
+      onRead={this.onSuccess}
+      flashMode={RNCamera.Constants.FlashMode.torch}
+    />
+    
+  );
+}
 
-  const Stack = createStackNavigator();
+const Stack = createStackNavigator();
 
-  const App = () => {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Details" component={DetailsScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-  }
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" headerMode={'none'}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 
 
-  export default App;
+export default App;
 
-   
