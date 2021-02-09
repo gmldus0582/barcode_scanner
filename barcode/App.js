481e5ff9cb91bf8dbc719d98fe82a9562f21c2f8
gmldus0582 <gmldus0582@gmail.com>
2021-02-09 14:20:59 +0900

    바코드 스캐너 인풋값 던지기 전

--- barcode/App.js
+++ barcode/App.js
@@ -1,96 +1,179 @@
-import React, { Component,useRef } from 'react';
-import { View, Text, Button,Dimensions, Alert } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { WebView } from 'react-native-webview';
-import { useRoute } from '@react-navigation/native';
+import React, { Component, useRef } from 'react';
+import { View, Text, Button, Dimensions, Alert, RefreshControl } from 'react-native';
 
 import {
-  AppRegistry,
-  StyleSheet,
-  TouchableOpacity,
-  Linking
+    AppRegistry,
+    StyleSheet,
+    TouchableOpacity,
+    Linking
 } from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import { RNCamera } from 'react-native-camera';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { useNavigation } from '@react-navigation/native';
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
+import { useState } from 'react';
+import { WebView } from 'react-native-webview';
 import { useEffect } from 'react';
-
+import { useRoute } from '@react-navigation/native';
 
 
 const charwidth = Dimensions.get('window').width;
-const charheight= Dimensions.get('window').height;
+const charheight = Dimensions.get('window').height;
 
-
-const NewHome = () => {
-  const navigation = useNavigation()
-
-  return(
-    <View>
-      <TouchableOpacity onPress={navigation.navigate('Home',{bar:'안녕'})}>
-      </TouchableOpacity>
-      </View>
-  )
+const wait = (timeout) => {
+    return new Promise(resolve => setTimeout(resolve, timeout));
 }
 
 
-const HomeScreen = ({route}) => {
-  const navigation = useNavigation()
 
-  useEffect(()=>{
-    if(route.params.bar == '안녕'){
-      Alert.alert('안녕')
-    }else{
-      Alert.alert(route.params.bar)
-      this.rnwebview.postMessage(route.params.bar);
-    }
-     },[route])
+function DetailsScreen() {
+    const navigation = useNavigation()
 
-  return (
+    onSuccess = e => {
+        Alert.alert(e.data)
+        navigation.navigate('ListPage', { num: e.data })
+    };
+    return (
+        <QRCodeScanner
+            onRead={onSuccess}
+            flashMode={RNCamera.Constants.FlashMode.torch}
+        />
+    );
+}
+
+var rnw;
+
+const HomeScreen = () => {
+    const navigation = useNavigation()
+
+
+    return (
         <WebView
-        ref = {wv =>{this.rnwebview = wv}}
-        onMessage={(event)=>navigation.navigate('Details',{wb:rnwebview})}
-        source={{uri: 'http://ip0139.cafe24.com/'}}
-        style={{marginTop: 20}}
-         
-      />
-
-      
-  );
+            ref={wv => { rnw = wv }}
+            onMessage={(event) => navigation.navigate('ListPage', { num: "안녕" })}
+            source={{ uri: 'http://ip0139.cafe24.com/' }}
+            style={{ marginTop: 20 }}
+        />
+    );
 }
+var list = []
 
-function DetailsScreen({route}) {
-  const navigation = useNavigation();
-  onSuccess = e => {
-    navigation.navigate('Home',{bar:e.data});
-  };
-  return (
-    
-    <QRCodeScanner
-      // onSuccess={(event)=>{ Linking.openURL(event.data)}}
-      onRead={onSuccess}
-      flashMode={RNCamera.Constants.FlashMode.torch}
-    />
-    
-  );
-  
+const ListPage = () => {
+
+    const [refreshing, setRefreshing] = React.useState(false);
+
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+        wait(1).then(() => setRefreshing(false));
+    }, []);
+
+
+    const navigation = useNavigation()
+    const route = useRoute()
+
+    useEffect(() => {
+        if (route.params.num == '안녕') {
+        } else {
+            list.push(route.params.num)
+            console.log(list)
+            onRefresh()
+        }
+    }, [route])
+
+    const Push = () => {
+        var pp = []
+        for (var i = 0; i < list.length; i++) {
+            pp.push(<Item key={i} num={list[i]}></Item>)
+        }
+        return pp
+    }
+
+    function delList(num) {
+        for (var i = 0; i < list.length; i++) {
+            if (list[i] == num) {
+                list.splice(i, 1);
+            }
+        }
+        console.log(list)
+        onRefresh()
+    }
+
+    function alldel() {
+        list = []
+        onRefresh()
+    }
+
+    function send(){
+      rnw.postMessage(list)
+      navigation.navigate('Home')
+    }
+
+    const Item = (prop) => {
+        return (
+            <View style={{ flexDirection: 'row' }}>
+                <View style={{ borderWidth: 0.5, width: charwidth - 60, height: 60 }}>
+                    <Text>{prop.num}</Text>
+                </View>
+                <TouchableOpacity onPress={() => { delList(prop.num), onRefresh() }}>
+                    <View style={{ width: 60, height: 60, backgroundColor: 'gray' }}>
+                        <Text>삭제</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+    return (
+        <View style={{ width: charwidth, height: charheight }}>
+            <ScrollView refreshControl={
+                <RefreshControl
+                    refreshing={refreshing}
+                    onRefresh={onRefresh}
+                />
+            }>
+
+                <Push new={route.params.num}></Push>
+            </ScrollView>
+
+
+            <TouchableOpacity onPress={() => send()}>
+                <View style={{ width: charwidth, height: 60, backgroundColor: 'blue', bottom: 0 }}>
+                    <Text>값전송</Text>
+                </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => alldel()}>
+                <View style={{ width: charwidth, height: 60, backgroundColor: 'blue', bottom: 0 }}>
+                    <Text>전체삭제</Text>
+                </View>
+            </TouchableOpacity>
+
+
+            <TouchableOpacity onPress={() => navigation.navigate('Details')}>
+                <View style={{ width: charwidth, height: 60, backgroundColor: 'gray', bottom: 0 }}>
+                    <Text>입력</Text>
+                </View>
+            </TouchableOpacity>
+        </View>
+    )
 }
 
 const Stack = createStackNavigator();
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="newhome" headerMode={'none'}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-        <Stack.Screen name="newhome" component={NewHome} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home" headerMode={'none'}>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Details" component={DetailsScreen} />
+                <Stack.Screen name="ListPage" component={ListPage} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 
 
 export default App;
-
