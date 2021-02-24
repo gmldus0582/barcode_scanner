11d55593787b6698c5763e3ef711ba3fa199ca29
gmldus0582 <gmldus0582@gmail.com>
2021-02-24 17:43:56 +0900

    바코드 구분하는 코드 추가

--- rncamera/App.js
+++ rncamera/App.js
@@ -1,5 +1,5 @@
-import React, { PureComponent } from 'react';
-import { Alert, AppRegistry, StyleSheet, Text, Dimensions, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { Alert, Dimensions, View, Text, TouchableOpacity, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 import { useNavigation } from '@react-navigation/native';
 import { NavigationContainer } from '@react-navigation/native';
@@ -10,70 +10,88 @@ const charwidth = Dimensions.get('window').width;
 const charheight = Dimensions.get('window').height;
 
 
-const bar = ({route}) => {
+const bar = ({ route }) => {
   const navigation = useNavigation()
+  var data = new Array();
   barcodeRecognized = ({ barcodes }) => {
-    barcodes.forEach(barcode =>{
-      if(barcode.data.length ==13){
-        Alert.alert(barcode.data);
-        route.params.rnw.postMessage(e.data);
+    barcodes.forEach(barcode => {
+      data = barcode.data.split("");
+      if (data[0] == 'w' || data[0] == 'W') {
+        //Alert.alert(barcode.data);
+        route.params.rnw.postMessage(barcode.data);
         navigation.navigate('Home')
       }
+      else if (data[0] == 'A' || data[0] == 'a') {
+        //Alert.alert(barcode.data);
+        route.params.rnw.postMessage(barcode.data);
+        navigation.navigate('Home')
+      }
+      else {
+        for (var i = 0; i <= 9; i++) {
+          if (Number(data[0]) == i) {
+            //Alert.alert(barcode.data);
+            route.params.rnw.postMessage(barcode.data);
+            navigation.navigate('Home')
+          }
+        }
+      }
+
     });
-    
+
   };
-  
+
 
   return (
-    <View style={{flex:1, flexDirection:'column',backgroundColor:'black'}} >
-      <View style={{width:charwidth,height:charheight, justifyContent:'center',alignItems:'center',}}>
-      
-      <RNCamera ratio={'2:3'}
-      type={RNCamera.Constants.Type.back}
-      flashMode={RNCamera.Constants.FlashMode.on}
-      style={{
-        height: charheight/7,
-        width:charwidth/1.5,
-        borderColor:'red',
-        alignItems:'center',
-        justifyContent:'center',
-      }}
-      zoom={1}
+    <View style={{ flex: 1, flexDirection: 'column', backgroundColor: 'white' }} >
+      <View style={{ width: charwidth, height: charheight, justifyContent: 'center', alignItems: 'center', }}>
+        <Text>스캔해주세요!</Text>
+        <RNCamera ratio={'2:3'}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          style={{
+            height: charheight / 7,
+            width: charwidth / 1.5,
+            borderColor: 'red',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+          zoom={0.8}
           onGoogleVisionBarcodesDetected={this.barcodeRecognized}
-       > 
-          <View style={{ width:charwidth/1.5, borderWidth:2, borderColor:'red'}}></View>
-        
-       </RNCamera>
-       
-       </View>
-       
+        >
+          <View style={{ width: charwidth / 1.5, borderWidth: 2, borderColor: 'red' }}></View>
+
+        </RNCamera>
+
+      </View>
+
     </View >
   );
 }
-
+var rnw;
+var rec;
 const HomeScreen = () => {
   const navigation = useNavigation()
+  
   return (
-      <View style={{width:charwidth,height:charheight}}>
-          <TouchableOpacity style={{width:charwidth,height:charheight}}>
-          <WebView
-              ref={wv => { rnw = wv }}
-              onMessage={(event) => {
-                  rec = event.nativeEvent.data;
-                  if(rec.length==5){
-                      navigation.navigate('bar', { rnw: rnw });
-                      Alert.alert("click")
-                  }
-                  else{
-                      rnw.postMessage("app!");
-                      Alert.alert("app!");
-                  }
-                  ;
-              }}
-              source={{ uri: 'http://ip0139.cafe24.com/' }}
-          ></WebView>
-          </TouchableOpacity>
-          </View>
+    <View style={{ width: charwidth, height: charheight }}>
+      
+      <WebView
+        ref={wv => { rnw = wv }}
+        onMessage={(event) => {
+          rec = event.nativeEvent.data;
+          if (rec.length == 5) {
+            navigation.navigate('bar', { rnw: rnw });
+          }
+          else {
+            rnw.postMessage("app!");
+            Alert.alert("app!");
+          }
+          ;
+        }}
+        source={{ uri: 'http://ip0139.cafe24.com/' }}
+      ></WebView>
+   
+    </View>
   );
 }
 
@@ -81,12 +99,12 @@ const Stack = createStackNavigator();
 
 const App = () => {
   return (
-      <NavigationContainer>
-          <Stack.Navigator initialRouteName="Home" headerMode={'none'}>
-              <Stack.Screen name="Home" component={HomeScreen} />
-              <Stack.Screen name="bar" component={bar} />
-          </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" headerMode={'none'}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="bar" component={bar} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 export default App;
