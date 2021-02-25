c78c4a8af0066335c1a4e8d8fb89d0ef1c507830
gmldus0582 <gmldus0582@gmail.com>
2021-02-25 15:45:40 +0900

    웹뷰 최소한의 코드

--- webview/App.js
+++ webview/App.js
@@ -1,24 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Alert,
-  Text,
-  StatusBar,
+    Dimensions,
+    StyleSheet,
+    View,
 } from 'react-native';
 import { WebView } from 'react-native-webview';
 import QRCodeScanner from 'react-native-qrcode-scanner';
-import { RNCamera } from 'react-native-camera';
 import { useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
@@ -26,50 +13,23 @@ import { NavigationContainer } from '@react-navigation/native';
 
 
 var rnw;
-var rec;
-
+const charwidth = Dimensions.get('window').width;
+const charheight = Dimensions.get('window').height;
 function NewBarcode({ route }) {
     const navigation = useNavigation()
-    //Alert.alert('new')
     onSuccess = e => {
-        //Alert.alert(e.data)
         route.params.rnw.postMessage(e.data);
         navigation.navigate('Home')
 
     };
-  return (
-      <QRCodeScanner
-          onRead={onSuccess}
-          flashMode={RNCamera.Constants.FlashMode.torch}
-          showMarker={true}
-          customMarker={
-              <View style={styles.case}>
-                  <View style={styles.caseIn} />
-              </View>
-          }
-      />
-  );
+    return (
+        <QRCodeScanner
+            onRead={onSuccess}
+            
+        />
+    );
 }
 
-const styles = StyleSheet.create({
-  case: {
-      width: "70%",
-      height: "40%",
-      borderColor: 'green',
-      borderWidth: 5,
-      justifyContent: "center",
-      alignItems: "center"
-  },
-  caseIn: {
-      width: "90%",
-      height: "3%",
-      marginBottom: 30,
-      marginTop: 30,
-      backgroundColor: '#FF0000',
-      justifyContent: "center",
-      alignItems: "center"
-  },
-});
 
 
 const HomeScreen = () => {
@@ -79,21 +39,12 @@ const HomeScreen = () => {
             ref={wv => { rnw = wv }}
             source={{ uri: 'http://ip0139.cafe24.com/' }}
             onMessage={(event) => {
-                rec = event.nativeEvent.data;
-                if (rec.length == 5) {
-                    navigation.navigate('new', { rnw: rnw });
-                    //Alert.alert("click")
-                }
-                if(rec.length ==3){
-                    navigation.navigate('new', { rnw: rnw });
-                    //Alert.alert("second")
-                }
-                ;
+                navigation.navigate('new', { rnw: rnw });
             }}
         />
-     
-  
-  );
+
+
+    );
 }
 
 const Stack = createStackNavigator();
