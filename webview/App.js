3eb75d5f43e64ab8dce73528428564f8769adef8
gmldus0582 <gmldus0582@gmail.com>
2021-02-26 09:19:05 +0900

    modal 사용

--- webview/App.js
+++ webview/App.js
@@ -1,64 +1,82 @@
-import React from 'react';
+import React, { useState } from "react";
 import {
     Dimensions,
-    StyleSheet,
+    Modal,
     View,
 } from 'react-native';
 import { WebView } from 'react-native-webview';
 import QRCodeScanner from 'react-native-qrcode-scanner';
-import { useNavigation } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-
+// import { useNavigation } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import { NavigationContainer } from '@react-navigation/native';
 
 
 var rnw;
 const charwidth = Dimensions.get('window').width;
 const charheight = Dimensions.get('window').height;
-function NewBarcode({ route }) {
-    const navigation = useNavigation()
-    onSuccess = e => {
-        route.params.rnw.postMessage(e.data);
-        navigation.navigate('Home')
+// function NewBarcode({ route }) {
+//     const navigation = useNavigation()
+//     onSuccess = e => {
+//         route.params.rnw.postMessage(e.data);
+//         navigation.navigate('Home')
 
-    };
-    return (
-        <QRCodeScanner
-            onRead={onSuccess}
-            
-        />
-    );
-}
+//     };
+//     return (
+//         <QRCodeScanner
+//             onRead={onSuccess}
+//             // showMarker={true}
+//             // customMarker={
+//             //     <View style={{width:charwidth/1.2, borderColor:'red', borderWidth:3, }}>
 
+//             //     </View>
+//             // }
+//         />
+//     );
+// }
 
 
 const HomeScreen = () => {
-    const navigation = useNavigation()
+    onSuccess = e => {
+        rnw.postMessage(e.data);
+        setModalVisible(false);
+    };
+    const [modalVisible, setModalVisible] = useState(false);
     return (
+        <View style={{width:charwidth,height:charheight,}}>
         <WebView
+            style={{width:charwidth,height:charheight,}}
             ref={wv => { rnw = wv }}
             source={{ uri: 'http://ip0139.cafe24.com/' }}
             onMessage={(event) => {
-                navigation.navigate('new', { rnw: rnw });
+                setModalVisible(true);
             }}
         />
-
+        <Modal visible = {modalVisible}>
+        <QRCodeScanner
+            onRead={onSuccess}
+            showMarker={true}
+            customMarker={
+                <View style={{width:charwidth/1.2, borderColor:'red', borderWidth:3, }}/>
+            }
+        />
+        </Modal>
+        </View>
 
     );
 }
 
-const Stack = createStackNavigator();
+//const Stack = createStackNavigator();
 
-const App = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home" headerMode={'none'}>
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="new" component={NewBarcode} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
-}
+// const App = () => {
+//     return (
+//         <NavigationContainer>
+//             <Stack.Navigator initialRouteName="Home" headerMode={'none'}>
+//                 <Stack.Screen name="Home" component={HomeScreen} />
+//                 <Stack.Screen name="new" component={NewBarcode} />
+//             </Stack.Navigator>
+//         </NavigationContainer>
+//     );
+// }
 
 
-export default App;
+export default HomeScreen;
