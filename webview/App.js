2377902500350ec18f4ad7be2ea56289f6eae382
HeeyeonPark <gmldus0582@naver.com>
2022-06-28 04:05:29 +0900

    Update App.js

--- webview/App.js
+++ webview/App.js
@@ -6,35 +6,10 @@ import {
 } from 'react-native';
 import { WebView } from 'react-native-webview';
 import QRCodeScanner from 'react-native-qrcode-scanner';
-// import { useNavigation } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import { NavigationContainer } from '@react-navigation/native';
-
 
 var rnw;
 const charwidth = Dimensions.get('window').width;
 const charheight = Dimensions.get('window').height;
-// function NewBarcode({ route }) {
-//     const navigation = useNavigation()
-//     onSuccess = e => {
-//         route.params.rnw.postMessage(e.data);
-//         navigation.navigate('Home')
-
-//     };
-//     return (
-//         <QRCodeScanner
-//             onRead={onSuccess}
-//             // showMarker={true}
-//             // customMarker={
-//             //     <View style={{width:charwidth/1.2, borderColor:'red', borderWidth:3, }}>
-
-//             //     </View>
-//             // }
-//         />
-//     );
-// }
-
-
 
 const HomeScreen = () => {
     onSuccess = e => {
@@ -51,7 +26,7 @@ const HomeScreen = () => {
             <WebView
                 style={{ width: charwidth, height: charheight, }}
                 ref={wv => { rnw = wv }}
-                source={{ uri: 'http://ip0139.cafe24.com/' }}
+                source={{ uri: 'MES 웹' }}
                 onMessage={(event) => {
                     setModalVisible(true);
                 }}
@@ -72,19 +47,6 @@ const HomeScreen = () => {
 
     );
 }
-
-//const Stack = createStackNavigator();
-
-// const App = () => {
-//     return (
-//         <NavigationContainer>
-//             <Stack.Navigator initialRouteName="Home" headerMode={'none'}>
-//                 <Stack.Screen name="Home" component={HomeScreen} />
-//                 <Stack.Screen name="new" component={NewBarcode} />
-//             </Stack.Navigator>
-//         </NavigationContainer>
-//     );
-// }
 
 
 export default HomeScreen;
